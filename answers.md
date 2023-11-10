b85802130d6aafcbe4e406ffea9fe78b102a0368
EllaMoses <emoses@tulane.edu>
2023-11-09 23:46:58 -0600

    Answers and Code

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Assignment 5
 ## Answers
 
-**Name:**_________________________
+**Name:** Ella Moses
 
 
 Place all written answers from `assignment-05.md` here for easier grading.
@@ -9,37 +9,54 @@ Place all written answers from `assignment-05.md` here for easier grading.
 
 
 
-- **1d.**
+- **1a.**
 
-File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
-----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
+Find the most valuable coin, largest power of 2, that is less than or equal to N. Subtract the value of that coin from N and repeat the process until zero is reached. Keep track of the coins used. 
+
+Start with value N and an empty list of coins
+
+While N > 0
+    coin_value = largest power of 2 less than or equal to N
+    add coin with value coin_value to list of coins
+    N - coinvalue
+
+At the end of the algorithm N is 0, return the list of coins to see how many coins and what values are necessary to make correct change. 
+
+- **1b.**
+
+ The optimal substructure property states that the optimal solution can be constructed from optimal solutions of smaller subproblems. This is true for this algorithm because the optimal solution for N is a set of coins C with the largest coin being C1, the subproblem of N - the value of C1 has the solution C without the first element. So, if we combine the results of these two subproblems, we get the optimal solution.
+
+The greedy choice property states that a greedy choice must be in some optimal solution (of a given size). Let G1-Gn be the coins in the greedy solution and O1-On be the coins in the optimal solution. Let i be the first index where the values in G and O differ. Assume that Gi < Oi. If G is not the optimal solution but we can replace a value in O with a value in G and make the total value smaller, then the original O was not optimal. This contradicts the idea that G is not equal to the optimal solution therefore G is equal to the optimal solution.  
 
 
+- **1c.**
 
-
-- **1d.**
-
-
+The work is O(nlogn) and span is O(n)
 
 
 - **2a.**
 
-
+Say a bank has the denominations {1,3,4,5} and we need to make change for 7. Using the greedy alorigthm from problem 1, we should take the most valuable denomination not exceeding N, in this case 5, and subtract it from 7. Now we have 2 remaining. Repeating this process, the most valuable coin not exceeding 2 is 1 so we subtract 1 from 2 and end up with 1. Repeat again to get to 0. This greedy algorithm results in us using 3 coins, {1,1,5} to make change for 7. This is not the optimal algorithm because it does not result in the optimal solution of 2 coins {3,4}. 
 
 
 - **2b.**
 
+The optimal substructure property states that the optimal solution can be constructed from optimal solutions of smaller subproblems. This is true for this algorithm because the optimal solution for N is a set of coins C with the largest coin being C1, the subproblem of N - the value of C1 has the solution C without the first element. So, if we combine the results of these two subproblems, we get the optimal solution.
 
 
 
-- **3b.**
+- **2c.**
 
 
+define a minimum coins function:
+
+starting value of min coins is inf
+
+While N > 0:
+for each denomination (Di) that is less than or equal to N:
+minimum_coins = min(minimum_coins, minimum_coins_fucntion(N-Di))
 
 
-- **3c.**
+The work and span are both O(n)
+
+
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from collections import Counter
 ####### Problem 3 #######
 
 test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant','-elephant'), ('AAAGAATTCA', 'AAA---T-CA')]
+alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('-ele-phant', 'relev--ant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
     # TO DO - modify to account for insertions, deletions and substitutions
@@ -19,13 +19,55 @@ def MED(S, T):
             return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
+
 def fast_MED(S, T, MED={}):
     # TODO -  implement top-down memoization
-    pass
+    if (S, T) in MED: #if the difference between these two strings has already been calculated (added to dictionary)
+        return MED[(S,T)] #return value of the (S, T) key in MED. Which is equal to the edit distance of these two strings
+    if (S == ""): #if S is an empty string the edit distance is equal to the length of T
+        MED[(S,T)] = len(T) #add key-value pair to the dictionary
+    elif (T == ""): #if T is an empty string the edit distance is equal to the length of S
+        MED[(S,T)] = len(S) #add key-value pair to the dictionary    
+    else:
+        if (S[0] == T[0]): #if characters are equal, the edit distance of S and T is equal to the edit distance of the remaining characters
+            MED[(S,T)] = fast_MED(S[1:], T[1:], MED)
+        else: #The edit distance is equal to 1 + the minimum of either an insertion or deletion
+            MED[S, T] = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED))
+    return MED[(S,T)] #return the edit distance for S and T
 
-def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+
+
+
+def fast_align_MED(S, T, MED={}, alignments={}):
+    # TODO -  keep track of alignments
+    if (S,T) in alignments:
+       return alignments[(S,T)] #if alignment has already been found, return it
+    if (S == ""):
+        MED[(S,T)] = len(T)
+        alignments[S,T] = ("-" * len(T), T) #if S is an empty string, T will remain unchanged and S will have len(T) insertions, represented by "-"
+    elif (T == ""):
+        MED[(S,T)] = len(S)
+        alignments[S,T] = (S, "-" * len(S)) #if T is an empty string, S will remain unchanged and T will have len(S) insertions, represented by "-"
+    else:
+        if (S[0] == T[0]):
+            MED[S, T] = fast_MED(S[1:], T[1:], MED) #same as fast_MED
+            alignments[S,T] = (S[0] + fast_align_MED(S[1:], T[1:], MED, alignments)[0], T[0] + fast_align_MED(S[1:], T[1:], MED, alignments)[1]) #if first characters are the same, leave the first character unchanged and find the alignment for the remaining characters
+        else:
+            #need to determine if it is more efficient to perform an insertion or deletion
+            if fast_MED(S[1:], T, MED) < fast_MED(S, T[1:], MED): #if deletion is more efficient than insertion
+                MED[S, T] = 1 + fast_MED(S[1:], T, MED) #cost calculation
+                #alignment: delete from S and insert to T, for deletions add "-", no edits for insertion
+                alignments[S,T] = (S[0] + fast_align_MED(S[1:], T, MED, alignments)[0], "-" + fast_align_MED(S[1:], T, MED, alignments)[1])
+            else: #if insertion is more efficient than deletion
+                MED[S,T] = 1 + fast_MED(S, T[1:], MED) #cost calculation
+                #alignment: delete from T and insert to S, for insertions add "-", no edits for deletion
+                alignments[S,T] = ("-" + fast_align_MED(S, T[1:], MED, alignments)[0], T[0] + fast_align_MED(S, T[1:], MED, alignments)[1])
+    return alignments[S,T]
+
+
+
+for S, T in test_cases:
+        print(fast_align_MED(S, T))
 
 def test_MED():
     for S, T in test_cases:
